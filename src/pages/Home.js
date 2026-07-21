1df78970d356f81452b82f2fde6b00c65926b952
Tharindu Dilshan Ekanayake <dilshantharindu8@gmail.com>
2026-07-21 20:54:18 +0530

    setup

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import SkinDiseasesCardCompo from '../components/SkinDiseasesCardCompo'
+
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+      <SkinDiseasesCardCompo />
+    </div>
+  )
+}
